c87780f221c5a87aaa2d00525ad81e57db574324
Mehtab Ali <128020370+EtherMan786@users.noreply.github.com>
2025-04-05 06:52:09 +0000

    Added all the dependecies

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,html}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: colors.blue,
+        secondary: colors.slate,
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+};
